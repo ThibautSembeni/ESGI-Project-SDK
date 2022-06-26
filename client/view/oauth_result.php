a53cf1497f74c1f056cfce9257c8064d4c62e0ed
Sembeni Thibaut <tsembeni@myges.fr>
2022-06-26 17:12:08 +0200

    :lipstick: add style for success page

--- client/view/oauth_result.php
+++ client/view/oauth_result.php
@@ -5,19 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projet - SDK | Login Sucess</title>
+    <link rel="stylesheet" href="./../dist/main.css">
 </head>
 <body>
-    <h1>Login result</h1>
-    <?php
 
-        $oauth = App\Model\Oauth::getInstance();
+    <main class="oauth-success">
+        <section>
+            <header>
+                <h1>Login result</h1>
+            </header>
+            <article>
+                <?php
 
-        if (empty($_SESSION)) 
-        {
-            header('Location: /' );
-        }
-        echo $oauth->getUser();
-        echo $oauth->getToken();
-    ?>
+                $oauth = App\Model\Oauth::getInstance();
+
+                if (empty($_SESSION)) 
+                {
+                    header('Location: /' );
+                } 
+                ?>
+                <h3>User :</h3>
+                <span><?= $oauth->getUser() ?></span>  
+                <h3>Acess Token :</h3> 
+                <span><?= $oauth->getToken() ?></span>
+            </article>
+            <footer>
+                <a href="/">Try another connexion Oauth</a>
+            </footer>
+        </section>
+    </main>   
 </body>
 </html>
